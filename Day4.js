2f5e9cfd2ee64b1d1e64d31e5a0dab04f33004e4
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-08-13 20:53:29 +0600

    implement day 4 problem

---
+++ Day4.js
@@ -0,0 +1,15 @@
+// Give a sequence of integer to an array, determine whether it is possible to obtain a strictly increasing sequence by removing not more than one element from the array.
+
+
+function almostIncreasingSequence(array) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] < array[i-1]) {
+            return false
+        }
+    }
+    return true
+}
+
+
+console.log(almostIncreasingSequence([1, 2, 3]))
+console.log(almostIncreasingSequence([1, 2, 1]))
