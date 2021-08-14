34746391f2ba71b49e84c63f97af3ac2eb8614fe
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-08-14 22:19:44 +0600

    grid traversal algorithms

---
+++ Dynamic_programming/grid-traversal.js
@@ -0,0 +1,23 @@
+// posibility of way to go in the right bottom node
+
+function gridTraversal(m, n, memo = {}) {
+    let key = m + ',' + n;
+
+    if(key in memo) return memo[key]
+
+    if(m === 1 && n === 1) {
+        return 1;
+    }
+    if(m === 0 || n === 0) {
+        return 0;
+    }
+
+    memo[key] = gridTraversal(m - 1, n, memo) + gridTraversal(m, n-1, memo);
+    return memo[key]
+}
+
+console.log(gridTraversal(1, 1));
+console.log(gridTraversal(2, 3));
+console.log(gridTraversal(3, 2));
+console.log(gridTraversal(3, 3));
+console.log(gridTraversal(18, 18));
