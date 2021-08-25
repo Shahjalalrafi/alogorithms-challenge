99257f5bd0acdc89068d6ae6f267efbb2c36427d
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-08-25 22:20:25 +0600

    characterCount problem solve

---
+++ javascript_algorithms/characterCount.js
@@ -0,0 +1,15 @@
+function characterCount(str) {
+    let character = {}
+    for(let i = 0; i< str.length; i++) {
+        let element = str[i].toLowerCase()
+       if(element !== ' ' && character[element] > 0) {
+           character[element] += 1
+       }else if(element !== ' '){
+           character[element] = 1
+       }
+    }
+    return character
+}
+
+console.log(characterCount('HeLlo everyOne how are you?'))
+
--- javascript_algorithms/timeOfOurCode.js
+++ javascript_algorithms/timeOfOurCode.js
@@ -1,25 +1,37 @@
-function addUpTo(n) {
-    let total = 0
+// function addUpTo(n) {
+//     let total = 0
+//     for(let i = 0; i< n; i++) {
+//         total += i
+//     }
+//     return total
+// }
+
+// let time1 = performance.now()
+// console.log(addUpTo(1000000000))
+// let time2 = performance.now()
+
+// console.log(`your performance time ${(time2 - time1)/1000}`)
+
+
+// // another approach
+// function addUpTo(n) {
+//     return n * (n+1) /2
+// }
+
+// let time1 = performance.now()
+// console.log(addUpTo(1000000000))
+// let time2 = performance.now()
+
+// console.log(`your performance time ${(time2 - time1)/1000}`)
+
+
+
+function printAllPairs(n) {
     for(let i = 0; i< n; i++) {
-        total += i
+        for(let j = 0; j < i; j++) {
+            console.log(i, j)
+        }
     }
-    return total
 }
 
-let time1 = performance.now()
-console.log(addUpTo(1000000000))
-let time2 = performance.now()
-
-console.log(`your performance time ${(time2 - time1)/1000}`)
-
-
-// another approach
-function addUpTo(n) {
-    return n * (n+1) /2
-}
-
-let time1 = performance.now()
-console.log(addUpTo(1000000000))
-let time2 = performance.now()
-
-console.log(`your performance time ${(time2 - time1)/1000}`)
+printAllPairs(3)
